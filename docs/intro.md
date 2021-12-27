6cb5742a786fa3cc6e55a6ffab3f8fb83c6adfeb
adamsoffer <adam@livepeer.org>
2021-12-27 16:56:28 -0500

    Initial commit

---
+++ docs/intro.md
@@ -0,0 +1,24 @@
+---
+slug: /
+sidebar_position: 1
+---
+
+# Introduction
+
+Welcome! The pages that follow contain comprehensive documentation of the
+Livepeer ecosystem. The documentation is split into two parts: Livepeer for
+[video developers](/docs/video-developers/overview) and Livepeer for
+[video miners](/docs/video-miners/overview). If you are new to Livepeer and
+interested in how it works, you might want to check out the
+[Protocol Overview](/docs/protocol/core-concepts/overview).
+
+The documentation is an open-source community effort, so feel free to suggest
+new topics, add new how-to guides, and provide examples wherever you think it
+might be helpful. All documentation is editable via
+[GitHub](https://github.com/livepeer/livepeer-org/tree/master/docs).
+
+## Something missing? {#something-missing}
+
+If you find issues with the documentation or have suggestions on how to improve the documentation or the project in general, please [file an issue](https://github.com/facebook/docusaurus) for us, or send a tweet mentioning the [@docusaurus](https://twitter.com/docusaurus) Twitter account.
+
+For new feature requests, you can create a post on our [feature requests board (Canny)](/feature-requests), which is a handy tool for roadmapping and allows for sorting by upvotes, which gives the core team a better indicator of what features are in high demand, as compared to GitHub issues which are harder to triage. Refrain from making a Pull Request for new features (especially large ones) as someone might already be working on it or will be part of our roadmap. Talk to us first!
---
+++ docusaurus.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Livepeer Docs",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      algolia: {
+        appId: "TIWEI9YB8Y",
+        apiKey: "7030b10ebe1bfc5913762385f6f1b6c7",
+        indexName: "livepeer-org-docs",
+        contextualSearch: true,
+      },
+      navbar: {
+        title: "Livepeer Docs",
+        logo: {
+          alt: "Livepeer Logo",
+          src: "img/logo.svg",
+        },
+        // items: [
+        //   {
+        //     type: "doc",
+        //     docId: "intro",
+        //     position: "left",
+        //     label: "Tutorial",
+        //   },
+        //   { to: "/blog", label: "Blog", position: "left" },
+        //   {
+        //     href: "https://github.com/facebook/docusaurus",
+        //     label: "GitHub",
+        //     position: "right",
+        //   },
+        // ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
