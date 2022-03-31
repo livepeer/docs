7341369c372ffefe398c3799ba2721e5f7941851
Hunter Hillman <hunterhillman@Hunters-MacBook-Pro.local>
2022-03-31 14:26:42 -0700

    bump packages and add breadcrumbs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
       ({
         docs: {
           routeBasePath: "/",
+          breadcrumbs: true,
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/livepeer/docs/blob/main",
         },
