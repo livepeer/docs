8453f93c9e31335dac959ccdc41402f635c7cba6
adamsoffer <adam@livepeer.org>
2022-12-15 10:38:21 -0500

    create index pages for folders inside guides

--- components/core/Card/index.tsx
+++ components/core/Card/index.tsx
@@ -4,10 +4,10 @@ function Card({ title, description, icon, href }: any) {
   return (
     <Link href={href}>
       <a>
-        <div className="p-4 group flex flex-col justify-start overflow-hidden rounded-lg border border-gray-200 bg-transparent text-current no-underline shadow-sm shadow-gray-100 transition-all duration-200 dark:border-neutral-700 dark:shadow-none hover:border-gray-300 hover:bg-slate-50 hover:shadow-md hover:shadow-gray-100 dark:hover:border-neutral-700 dark:hover:bg-neutral-900 dark:hover:shadow-none active:shadow-sm active:shadow-gray-200">
+        <div className="h-full p-4 group flex flex-col justify-start overflow-hidden rounded-lg border border-gray-200 bg-transparent text-current no-underline shadow-sm shadow-gray-100 transition-all duration-200 dark:border-neutral-700 dark:shadow-none hover:border-gray-300 hover:bg-slate-50 hover:shadow-md hover:shadow-gray-100 dark:hover:border-neutral-700 dark:hover:bg-neutral-900 dark:hover:shadow-none active:shadow-sm active:shadow-gray-200">
           <div className="mb-2 text-lg">{icon}</div>
           <div>
-            <div className="text-lg mb-1 font-bold">{title}</div>
+            <h3 className="text-lg mb-1 font-bold">{title}</h3>
             <div>{description}</div>
           </div>
         </div>
@@ -20,9 +20,10 @@ export function Cards({ children, num, ...props }: any) {
   return (
     <div
       style={{
-        marginTop: 4,
+        marginTop: 20,
         gap: '16px',
         display: 'grid',
+        gridAutoRows: '1fr',
         '--rows': num || 3,
         gridTemplateColumns:
           'repeat(auto-fill, minmax(max(250px, calc((100% - 1rem * 2) / var(--rows))), 1fr))',
