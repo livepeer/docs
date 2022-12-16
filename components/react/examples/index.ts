04235fdc2c93f5b8b960a02c1e717a357c0ada84
Chase Adams <me@0xcadams.com>
2022-12-15 18:25:55 -0700

    Add developer guides (#268)

--- components/react/examples/index.ts
+++ components/react/examples/index.ts
@@ -3,5 +3,7 @@ export * from './AptosNft';
 export * from './Asset';
 export * from './DecentralizedStoragePlayback';
 export * from './DemoPlayer';
+export * from './IframePlayer';
 export * from './Stream';
+export * from './VideoJSPlayer';
 export * from './WagmiNft';
