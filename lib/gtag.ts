d3b170533c99fd31908444f61015a3f04657badc
adamsoffer <adam@livepeer.org>
2023-01-17 15:39:47 -0500

    fix typescript errs

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,17 +2,22 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  // @ts-ignore
-  window.gtag('config', GA_TRACKING_ID, {
+  (<any>window).gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// @ts-ignore
-export const event = ({ action, category, label, value }) => {
-  // @ts-ignore
-  window.gtag('event', action, {
+
+interface EventProps {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+}
+
+export const event = ({ action, category, label, value }: EventProps) => {
+  (<any>window).gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
