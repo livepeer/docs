c7580a9f974c6e5e15d8c2a9338abe1944b22309
adamsoffer <adam@livepeer.org>
2023-01-17 15:33:57 -0500

    add GA

---
+++ lib/gtag.ts
@@ -0,0 +1,20 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  // @ts-ignore
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+// @ts-ignore
+export const event = ({ action, category, label, value }) => {
+  // @ts-ignore
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
