982ccb0100620b97f83f65b2836f8a26d5e45339
Chase Adams <c@cadams.io>
2023-01-25 11:03:17 -0700

    fix: link

--- components/core/Link.tsx
+++ components/core/Link.tsx
@@ -7,10 +7,11 @@ type Props = {
 };
 
 const Link = ({ href, children }: Props) => (
-  <NextLink href={href}>
-    <a className="flex nx-text-sm contrast-more:nx-text-gray-700 contrast-more:dark:nx-text-gray-100 -nx-ml-2 nx-hidden nx-whitespace-nowrap nx-p-2 md:nx-inline-block nx-relative nx-text-gray-600 hover:nx-text-gray-800 dark:nx-text-gray-400 dark:hover:nx-text-gray-200">
-      {children}
-    </a>
+  <NextLink
+    className="flex nx-text-sm contrast-more:nx-text-gray-700 contrast-more:dark:nx-text-gray-100 -nx-ml-2 nx-hidden nx-whitespace-nowrap nx-p-2 md:nx-inline-block nx-relative nx-text-gray-600 hover:nx-text-gray-800 dark:nx-text-gray-400 dark:hover:nx-text-gray-200"
+    href={href}
+  >
+    {children}
   </NextLink>
 );
 
