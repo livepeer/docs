013d86979cd66501cfe934f878532d7555b9d796
Suhail Kakar <suhail.zahed@gmail.com>
2023-03-07 13:44:01 +0530

    docs: added IPFS and 4Everland tutorial

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -26,8 +26,7 @@ export default async function handler(req: NextRequest) {
           flexDirection: 'column',
           alignItems: 'flex-start',
           justifyContent: 'center',
-          backgroundImage:
-            'url(https://res.cloudinary.com/doxkn6teg/image/upload/v1677773730/bg_ixxj23.jpg)',
+          backgroundImage: 'url(https://docs.livepeer.org/og-bg.jpg)',
           paddingLeft: 135,
           paddingRight: 450,
         }}
