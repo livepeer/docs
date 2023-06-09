5226750b5bd45ba3b996b933c1dd1df151c0505a
Suhail Kakar <suhail.zahed@gmail.com>
2023-06-09 19:19:15 +0530

    feat: add multi language code snippet for request (#398)

--- components/openapi/Request.tsx
+++ components/openapi/Request.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import useCodeSnippets from 'hooks/openapi/useCodeSnippets';
+
 import { ParameterInfo } from './Parameters';
 
 import Pre from './shared/Pre';
@@ -11,80 +13,22 @@ interface RequestProps {
   path: string;
 }
 
-const Request: React.FC<RequestProps> = ({ baseUrl, params, method, path }) => {
-  const buildNestedObject = (
-    properties: ParameterInfo[],
-  ): { [key: string]: any } => {
-    const nestedObject: { [key: string]: any } = {};
-
-    properties.forEach((property: ParameterInfo) => {
-      nestedObject[property.property] = property.example || null;
-
-      if (property.objectProperties && property.objectProperties.length > 0) {
-        const nestedNestedObject = buildNestedObject(property.objectProperties);
-
-        if (Object.keys(nestedNestedObject).length > 0) {
-          nestedObject[property.property] = nestedNestedObject;
-        }
-      }
-    });
-
-    return nestedObject;
-  };
-
-  const generateCurlCommand = (): string => {
-    const curlCommand = `curl -X ${method.toUpperCase()} -H 'content-type: application/json'`;
-
-    const requestBody: { [key: string]: any } = {};
-
-    params?.forEach((property) => {
-      if (property.property && property.example) {
-        requestBody[property.property] = property.example;
-      }
-
-      if (property.objectProperties && property.objectProperties.length > 0) {
-        const nestedObject = buildNestedObject(property.objectProperties);
-
-        if (Object.keys(nestedObject).length > 0) {
-          requestBody[property.property] = nestedObject;
-        }
-      }
-
-      if (
-        property.array &&
-        property.arraySchema &&
-        property.arraySchema.length > 0
-      ) {
-        const nestedArray: { [key: string]: any }[] = [];
-        property.arraySchema.forEach((nestedItem: ParameterInfo) => {
-          const nestedObject: { [key: string]: any } = {};
-          nestedItem.objectProperties?.forEach((nestedProperty) => {
-            if (nestedProperty.property && nestedProperty.example) {
-              nestedObject[nestedProperty.property] = nestedProperty.example;
-            }
-          });
-          if (Object.keys(nestedObject).length > 0) {
-            nestedArray.push(nestedObject);
-          }
-        });
-        if (nestedArray.length > 0) {
-          requestBody[property.property] = nestedArray;
-        }
-      }
-    });
-
-    const requestBodyString =
-      method !== 'get' && JSON.stringify(requestBody, null, 2);
-    const requestUrl = baseUrl ? `${baseUrl}${path}` : path;
-    return `${curlCommand} -d '${requestBodyString}' ${requestUrl}`;
-  };
-
-  const sampleRequest = generateCurlCommand();
+const Request: React.FC<RequestProps> = ({
+  baseUrl,
+  params,
+  method,
+  path,
+}: RequestProps) => {
+  const codeSnippets = useCodeSnippets(baseUrl, params, method, path);
 
   return (
-    <Pre dark method={method} filename={path}>
-      {sampleRequest}
-    </Pre>
+    <Pre
+      dark
+      method={method}
+      filename={path}
+      fromRequest={true}
+      requestSamples={codeSnippets}
+    />
   );
 };
 
