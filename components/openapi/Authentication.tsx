b59101a2bb5f54ddfacac079e6ce96a81b53dc35
Suhail <suhail.zahed@gmail.com>
2023-07-19 01:50:59 +0530

    add info regarding cors

--- components/openapi/Authentication.tsx
+++ components/openapi/Authentication.tsx
@@ -3,6 +3,7 @@ import { Code } from 'nextra/components';
 import React from 'react';
 
 import Pre from './shared/Pre';
+import Link from 'next/link';
 
 const Authentication: React.FC = () => {
   return (
@@ -14,18 +15,38 @@ const Authentication: React.FC = () => {
         <div className=" lg:w-1/2">
           <h2 className="font-medium text-2xl">Authentication</h2>
           <p className="mt-4">
-            Livepeer API uses API keys to verify and authorize requests. You can
-            manage and review your API keys through Livepeer Studio.
-            <br /> <br />
-            You need to pass your API key in the header with <Code>
-              Bearer
-            </Code>{' '}
-            prefix while sending a request
-            <br /> <br />
+            Livepeer API uses API keys (
+            <Link
+              href={
+                'https://swagger.io/docs/specification/authentication/bearer-authentication/'
+              }
+              className="dark:text-green-400 text-blue-500"
+            >
+              Bearer Authentication
+            </Link>
+            ) to verify and authorize requests. You can manage and review your
+            API keys through Livepeer Studio. You need to pass your API key in
+            the header with <Code>Bearer</Code> prefix while sending a request
+          </p>
+          <p className="mt-4">
             It's important to note that your API keys come with significant
             privileges, so it's essential to keep them safe and secure! Refrain
             from sharing your secret API keys in GitHub or other publicly
             accessible places
+          </p>
+          <p className="mt-4">
+            By default, API keys can only be used from a backend server. This is
+            to ensure maximum security and prevent that you accidentally expose
+            your account by including the secret API key in some public web
+            page. More info about enabling CORS can be found{' '}
+            <Link
+              className="dark:text-green-400 text-blue-500"
+              href={
+                'https://docs.livepeer.org/guides/developing/quickstart.en-US#enable-cors-optional'
+              }
+            >
+              here.
+            </Link>
           </p>
         </div>
         <div className="mt-2 lg:w-2/5 lg:ml-20">
