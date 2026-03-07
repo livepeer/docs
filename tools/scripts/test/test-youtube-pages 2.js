9760a557543607695956114466d0957d36d18492
AlisonWonderland <ally@lilypad.tech>
2026-03-07 18:13:14 +1100

    update allowlist

--- tools/scripts/test/test-youtube-pages 2.js
+++ tools/scripts/test/test-youtube-pages 2.js
@@ -42,7 +42,7 @@ const testPages = [
   '/home/trending',
   '/home/copy-trending-at-livepeer',
   '/about-livepeer/moved/livepeer-overview',
-  '/platforms/portal',
+  '/solutions/portal',
   '/livepeer-community/trending-topics',
 ];
 
