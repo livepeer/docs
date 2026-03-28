6e774185853b3011679e26674cbdeb2aa781b0b8
AlisonWonderland <ally@lilypad.tech>
2026-03-28 12:01:29 +1100

    solutions completed

--- v2/solutions/streamplace/data/socials.jsx
+++ v2/solutions/streamplace/data/socials.jsx
@@ -1,6 +1,7 @@
 export const streamplaceSocials = [
   { icon: 'globe',      href: 'https://stream.place/',                        label: 'Streamplace' },
   { icon: 'file-code',  href: 'https://stream.place/docs/',                   label: 'Streamplace Docs' },
+  { icon: 'gitlab',     href: 'https://git.stream.place/streamplace/streamplace', label: 'Streamplace GitLab' },
   { icon: 'github',     href: 'https://github.com/streamplace/streamplace',   label: 'Streamplace GitHub' },
   { icon: 'tower-broadcast', href: 'https://stream.place/live',               label: 'Live Dashboard' },
   { icon: 'discord',    href: 'https://discord.com/invite/EdtZv4UTMU',        label: 'Streamplace Discord' },
